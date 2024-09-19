ebdf864c302ef6c1785846ad3d27d2ef1f76068e
Manuel Palmero <manueligna06@gmail.com>
2024-09-19 19:27:53 -0300

    Actualizado ejercicio 17 y documentadas las funciones

--- Ejercicio-17.py
+++ Ejercicio-17.py
@@ -5,20 +5,32 @@
 from num2words import num2words
 
 meses = {
-            "01": "Enero",
-            "02": "Febrero",
-            "03": "Marzo",
-            "04": "Abril",
-            "05": "Mayo",
-            "06": "Junio",
-            "07": "Julio",
-            "08": "Agosto",
-            "09": "Setiembre",
-            "10": "Octubre",
-            "11": "Noviemre",
-            "12": "Diciembre",
-        }
-def diaExisteEnMes(d, m, a):
+    "01": "Enero",
+    "02": "Febrero",
+    "03": "Marzo",
+    "04": "Abril",
+    "05": "Mayo",
+    "06": "Junio",
+    "07": "Julio",
+    "08": "Agosto",
+    "09": "Setiembre",
+    "10": "Octubre",
+    "11": "Noviemre",
+    "12": "Diciembre",
+}
+
+
+def diaExisteEnMes(d, m, a) -> bool:
+    """Función para comprobar la existencia de un día dentro de un mes determinado
+
+    Argumentos:
+        d: día
+        m: mes
+        a: año
+
+    Devuelve:
+        bool: Existe el día (True) o no (False)
+    """
     d = int(d)
     m = int(m)
     a = int(a)
@@ -28,43 +40,47 @@ def diaExisteEnMes(d, m, a):
         case 4 | 6 | 9 | 11:
             dias = 30
         case 2:
-            if (a % 4 == 0 and (a % 100 != 0 or a % 400 == 0)):
+            if a % 4 == 0 and (a % 100 != 0 or a % 400 == 0):
                 dias = 29
             else:
                 dias = 28
-        case _:
-            dias = 2
     return 1 <= d <= dias
-    
-class FuncionesPrograma:
 
+
+class FuncionesPrograma:
     @staticmethod
     def getFechaString(f) -> str:
-        
-        if f[2] == '/' and f[5] == '/' and len(f) == 10:
+        """Función que expresa una fecha dada en formato DD/MM/AAAA como una fecha en palabras
+
+        Argumentos:
+            f (str): Fecha dada
+        """
+        if f[2] == "/" and f[5] == "/" and len(f) == 10:
             dd: str = f[0:2]
             mm: str = f[3:5]
             aaaa: str = f[6:10]
-            
+
             if 1 <= int(mm) <= 12:
                 m = meses[mm]
             else:
                 return f"{mm} no es un mes."
-            
+
             if diaExisteEnMes(dd, mm, aaaa):
-                d = num2words(dd, lang='es')
+                if dd == "01":
+                    d = "Primero"
+                else:
+                    d = num2words(dd, lang="es")
             else:
                 return f"No existe el día {dd} en {m}"
             d = d.capitalize()
-            
             m = m.capitalize()
-            
-            a = num2words(aaaa, lang='es')
+            a = num2words(aaaa, lang="es")
 
             return f"{d} de {m} de {a}."
         else:
             return f"{f} no está en formato DD/MM/AAAA."
-        
+
+
 funciones = FuncionesPrograma()
 
 fecha = str(input("Ingrese una fecha en formato DD/MM/AAAA: "))
