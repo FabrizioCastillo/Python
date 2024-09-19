59866b120a2c122f647027a25b4a49e866ab03be
Manuel Palmero <manueligna06@gmail.com>
2024-09-19 20:01:30 -0300

    Añadida la consigna

---
+++ Ejercicio-18.py
@@ -0,0 +1,7 @@
+"""
+En la clase FuncionesPrograma codifique una método getFechaDate estática que
+reciba como parámetro 3 valores enteros, día, mes, anio y retorne la fecha de tipo
+date correspondiente.
+
+En la clase Principal creada en el punto anterior haga uso de la función getFechaDate.
+"""
