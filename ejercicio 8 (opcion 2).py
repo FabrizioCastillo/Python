24a5bdd0593d70a782a0271b35b822ccf64066c6
UrielRojas04 <jaurojas2018@gmail.com>
2024-09-20 00:01:37 -0300

    Create ejercicio 8 (opcion 2).py

---
+++ ejercicio 8 (opcion 2).py
@@ -0,0 +1,4 @@
+frase=input("Ingrese una frase: ")
+frase=frase.lower()
+nuevaFrase=frase.replace("a","e")
+print(f"El resultado de la frase intercambiando sus letras 'a' por 'e' es: {nuevaFrase}" )
