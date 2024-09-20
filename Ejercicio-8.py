db0d38e9be0adb7fa5f3d1c458fb4c5b8e393d81
Blinnnnd <jmluna200420@gmail.com>
2024-09-20 08:22:42 -0300

    definitivo8.py

--- Ejercicio-8.py
+++ Ejercicio-8.py
@@ -1,4 +1,4 @@
 palabra = input("Ingrese una palabra: ")
-cambio = palabra.replace("a", "e").replace("i", "e").replace("o", "e").replace("u", "e")
+cambio = palabra.replace("a", "e").replace("A", "E")
 print(f"La palabra modificada es: {cambio}")
 
