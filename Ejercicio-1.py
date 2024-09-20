4efa465f3e63a95840bfc9a5a478b01af0c75926
Manuel Palmero <manueligna06@gmail.com>
2024-09-20 00:08:19 -0300

    bugfix

--- Ejercicio-1.py
+++ Ejercicio-1.py
@@ -1,14 +1,14 @@
-short_min = -32,768
-Short_max = 32,767
-Long_min = -9223372036854775808
-Long_max = 9223372036854775807
+SHORT_MIN = -32.768
+SHORT_MAX = 32.767
+LONG_MIN = -9223372036854775808
+LONG_MAX = 9223372036854775807
 def short(num):
-    if short_min  <= num <= Short_max:
+    if SHORT_MIN  <= num <= SHORT_MAX:
         print(f"Valor convertido a short: {int(num)}")
     else:
         print("El valor está fuera del rango de short.")
 def long(num):
-    if Long_min <= num <= Long_max:
+    if LONG_MIN <= num <= LONG_MAX:
         print(f"Valor convertido a long: {int(num)}")
     else:
         print("El valor esta fuera del rango de long.")
